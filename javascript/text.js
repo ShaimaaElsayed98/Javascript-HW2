b685674e8ead1d19042e7bccef509b2c559cb714
ShaimaaElsayed98 <53016795+ShaimaaElsayed98@users.noreply.github.com>
2019-08-30 19:14:27 +0300

    Add files via upload

---
+++ javascript/text.js
@@ -0,0 +1,26 @@
+console.log("Hello World!");
+console.log("Γεια, κόσμε");
+console.log("مرحبا, يا عالم");
+
+console.log("I'm awesome");
+let x;
+console.log("the value of x will be: a number");
+console.log("the value of x wil be : 20");
+x=20;
+console.log("the value of x will be : 20");
+console.log(x)
+let y="hello";
+console.log("the value of my string will be: hello");
+console.log(y);
+y="hello everybody";
+console.log("the value of my string will be : hello eveybody");
+console.log(y);
+
+const fruits = [];
+console.log("the value of the fruits is : banana , apples , cherries");
+console.log(fruits);
+ 
+const favAnimals = ["dogs", "hamsters", "cats"]
+console.log(favAnimals);
+favAnimals.push ("baby pig");
+console.log(favAnimals)
